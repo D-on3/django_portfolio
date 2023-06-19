0524f53e8d209dc65a7d1538485c12ada7bd1802
D-on3 <104354779+D-on3@users.noreply.github.com>
2023-06-19 07:20:21 +0300

    adding login

--- portfolio_web/login/models.py
+++ portfolio_web/login/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    # Add your custom fields here
+
+    class Meta:
+        db_table = 'auth_user'
+        swappable = 'AUTH_USER_MODEL'
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
